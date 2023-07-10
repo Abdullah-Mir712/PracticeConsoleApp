ab62e94cf98a7f84183e1fbef5624d819931c4c3
4850 <4850@CMDLHRLT998>
2023-07-10 06:13:42 -0400

    Add project files.

---
+++ PracticeConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PracticeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int n = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= n)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("-");
+                }
+                else
+                {
+                    Console.WriteLine("+");
+                }
+
+                i++;
+            }
+           
+        }
+    }
+}
