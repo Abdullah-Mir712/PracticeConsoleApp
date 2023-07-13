947567c670548722cd1f2018cb4e1fdb646cd87e
4850 <4850@CMDLHRLT998>
2023-07-13 07:20:20 -0400

    version 4 and zoo mngmnt

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -4,24 +4,43 @@ namespace PracticeConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter a number");
-            int n = int.Parse(Console.ReadLine());
-            int i = 1;
-            while (i <= n)
+       
+            static public int Solution(string S)
             {
-                if (i % 2 == 0)
+                int upCount = 0;
+                int downCount = 0;
+                int leftCount = 0;
+                int rightCount = 0;
+
+                foreach (char arrow in S)
                 {
-                    Console.WriteLine("-");
-                }
-                else
-                {
-                    Console.WriteLine("+");
+                    switch (arrow)
+                    {
+                        case '^':
+                            upCount++;
+                            break;
+                        case 'v':
+                            downCount++;
+                            break;
+                        case '<':
+                            leftCount++;
+                            break;
+                        case '>':
+                            rightCount++;
+                            break;
+                    }
                 }
 
-                i++;
+                int maxCount = Math.Max(Math.Max(upCount, downCount), Math.Max(leftCount, rightCount));
+                int minRotations = S.Length - maxCount;
+
+                return minRotations;
             }
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Solution("<<<<^^"));
            
         }
     }
